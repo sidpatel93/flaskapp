5b1d66db41de0d83b2ea60e3da37e8083bf8e5c6
Sid Patel <sid.auburn@gmail.com>
2021-07-22 10:29:22 -0500

    working on templates and added demo html&CSS code

--- market.py
+++ market.py
@@ -1,11 +1,9 @@
-from flask import Flask
+from flask import Flask, render_template
+
 
 app = Flask(__name__)
 
 @app.route("/")
-def hello_world():
-    return "<h2>Hello, World!</h2>"
-
-@app.route("/about/<username>")
-def about_page(username):
-    return f'<h1>This is about page for {username} </h1>'
+@app.route("/home")
+def home_page():
+    return render_template('home.html')
