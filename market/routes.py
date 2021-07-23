c472ce6d34464576ec7f78f94ba39339fed1ec28
Sid Patel <sid.auburn@gmail.com>
2021-07-23 12:40:02 -0500

    added flash messages for the registration validation

--- market/routes.py
+++ market/routes.py
@@ -1,5 +1,5 @@
 from market import app, db
-from flask import render_template, redirect, url_for
+from flask import render_template, redirect, url_for, flash
 from market.models import Item, User
 from market.forms import RegisterForm
 
@@ -30,5 +30,6 @@ def register_page():
 
     if form.errors != {}:  # if the the form.error dictionary is not empty
         for err_msg in form.errors.values():
-            print(f'There is an error with creating a user: {err_msg}')
+            flash(f'There is an error with creating a user: {err_msg}', category='danger')
+
     return render_template('register.html', form=form)
