9503bcde58d0d22c4447a7d30cc4700cb4ae6dab
Sid Patel <sid.auburn@gmail.com>
2021-07-25 08:58:51 -0500

    added login page&form. Added the links in the register and login pages

--- market/forms.py
+++ market/forms.py
@@ -3,6 +3,7 @@ from wtforms import StringField, PasswordField, SubmitField
 from wtforms.validators import Length, EqualTo, Email, DataRequired, ValidationError
 from market.models import User
 
+
 class RegisterForm(FlaskForm):
 
     def validate_username(self, username_to_check):
@@ -20,3 +21,9 @@ class RegisterForm(FlaskForm):
     password1 = PasswordField(label='Password:', validators=[Length(min=6), DataRequired()])
     password2 = PasswordField(label='Confirm Password:', validators=[EqualTo('password1'), DataRequired()])
     submit = SubmitField(label='Create Account')
+
+
+class LoginForm(FlaskForm):
+    username = StringField(label='UserName:', validators=[DataRequired()])
+    password = PasswordField(label='Password', validators=[DataRequired()])
+    submit = SubmitField(label='Sign In')
