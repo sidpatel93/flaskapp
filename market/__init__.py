0393dc1e6e0eb3c33d0ead91e2fe11ecb75be08d
Sid Patel <sid.auburn@gmail.com>
2021-07-25 08:28:58 -0500

    added the password encription when it is stored in DB

--- market/__init__.py
+++ market/__init__.py
@@ -1,9 +1,12 @@
-from flask import Flask, render_template
+from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
+from flask_bcrypt import Bcrypt
 
 app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///market.db'
 app.config['SECRET_KEY'] = 'ebde1ed8ed186120d8bd7183'
 db = SQLAlchemy(app)
+bcrypt = Bcrypt(app)
+
 
 from market import routes
