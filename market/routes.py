264f3be18aac7bb18b65add90399ffba426d5601
Sid Patel <sid.auburn@gmail.com>
2021-07-27 07:08:10 -0500

    implemened logout function and added detail for home page

--- market/routes.py
+++ market/routes.py
@@ -2,7 +2,7 @@ from market import app, db
 from flask import render_template, redirect, url_for, flash
 from market.models import Item, User
 from market.forms import RegisterForm, LoginForm
-from flask_login import login_user
+from flask_login import login_user, logout_user
 
 @app.route("/")
 @app.route("/home")
@@ -46,3 +46,9 @@ def login_page():
         else:
             flash('Username and Password do not match', category='danger')
     return render_template('login.html', form=form)
+
+@app.route("/logout")
+def logout_page():
+    logout_user()
+    flash("You have been loggedout", category="info")
+    return redirect(url_for('home_page'))
